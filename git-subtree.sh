9ab4602f9443fd0b7cfdf7314feb162528c99f3f
Michael Dwan <mpdwan@gmail.com>
2012-06-12 10:13:06 -0600

    hardcoding --force on push.

--- git-subtree.sh
+++ git-subtree.sh
@@ -703,7 +703,7 @@ cmd_push()
 	    repository=$1
 	    refspec=$2
 	    echo "git push using: " $repository $refspec
-	    git push $repository $(git subtree split --prefix=$prefix):refs/heads/$refspec
+	    git push --force $repository $(git subtree split --prefix=$prefix):refs/heads/$refspec
 	else
 	    die "'$dir' must already exist. Try 'git subtree add'."
 	fi
